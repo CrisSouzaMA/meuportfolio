db9021802c2736d9361ba09f48439af0571621aa
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-04-16 12:45:54 -0300

    Tentando ajeitar rota

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 
 ReactDOM.render(
-  <HashRouter>
+  <HashRouter hashType='hashbang'>
     <App />
   </HashRouter>,
   document.getElementById('root')
